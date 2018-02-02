abbfaabbfc72660d9bc90acfc6b4e27071aa7060
Pierre Genty <gentyp@hotmail.fr>
2018-02-02 09:07:52 +0100

    first commit

---
+++ WesterosWikiScraper/FileManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SampleScraperClient
+{
+    public class FileManager
+    {
+        public static void WriteCsv(string pFilePath, List<string> pToWrite)
+        {
+            using(var w = new StreamWriter(pFilePath, append:true))
+            {
+                try
+                {
+                    var csv = new StringBuilder();
+
+                    csv.Append("#" + pToWrite.FirstOrDefault() + "\n");
+
+                    pToWrite.RemoveAt(0);
+
+                    foreach (var line in pToWrite)
+                    {
+                        csv.Append(line);
+                    }
+
+                    w.WriteLine(csv);
+                    w.Flush();
+                }
+                catch(Exception e)
+                {
+                    var eMess = e.Message;
+                    //do nothing if you can not write
+                }
+
+            }
+           
+        }
+
+        public static List<string> StoreUriListFromXml(string pfilePath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(pfilePath);
+
+            var uriList = new List<string>();
+            XmlNodeList elemList = doc.GetElementsByTagName("loc");
+            for(int i = 0; i < elemList.Count; i++)
+            {
+                uriList.Add(elemList[i].InnerText);
+            }
+
+            return uriList;
+        }
+    }
+}
---
+++ WesterosWikiScraper/Scraper.cs
@@ -0,0 +1,60 @@
+﻿using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SampleScraperClient
+{
+    public class Scraper
+    {
+        public static void ScrapMultipleUri(List<string> pUriList)
+        {
+            for(int i = 0; i < pUriList.Count(); i++)
+            {
+                FileManager.WriteCsv("C:\\Users\\Pierre\\Documents\\WesterosWikiScraper\\WesterosWikiScraper\\database.csv", ScrapUrl(pUriList.ElementAt(i)));
+            }
+        }
+
+        public static List<string> ScrapUrl(string pUri)
+        {
+            try
+            {
+                ScrapingBrowser Browser = new ScrapingBrowser();
+                Browser.AllowAutoRedirect = true;
+                Browser.AllowMetaRedirect = true;
+                WebPage PageResult = Browser.NavigateToPage(new Uri(pUri));
+
+                HtmlNode TitleNode = PageResult.Html.CssSelect("#firstHeading").First();
+                string PageTitle = TitleNode.InnerText;
+
+                var plainText = PageResult.Html.CssSelect("#mw-content-text");
+                var nodes = plainText.First().SelectNodes("p").ToList();
+                var rawHtml = new List<string>();
+
+                rawHtml.Add(PageTitle);
+
+                foreach (var row in nodes)
+                {
+                    rawHtml.Add(Strip(row.InnerText));
+                }
+
+                return rawHtml;
+            }
+            catch(Exception e)
+            {
+                var exceptionmessage = e.Message;
+                return new List<string>();
+            }
+        }
+
+        private static string Strip(string text)
+        {
+            return Regex.Replace(text, @"\[.*\]", string.Empty);
+        }
+    }
+}
